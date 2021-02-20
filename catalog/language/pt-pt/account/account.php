6a8acfe221256045a2ff495eafb69c8f2c555861
Marco Ribeiro <marco.lob@gmail.com>
2021-02-20 15:37:51 +0000

    added pt-pt folders

---
+++ catalog/language/pt-pt/account/account.php
@@ -0,0 +1,26 @@
+<?php
+// Heading
+$_['heading_title']       = 'Minha conta';
+
+// Text
+$_['text_account']        = 'Minha conta';
+$_['text_my_account']     = 'Minha conta';
+$_['text_my_orders']      = 'Meus pedidos';
+$_['text_my_affiliate']   = 'A Minha Conta de Afiliado';
+$_['text_my_newsletter']  = 'Newsletter';
+$_['text_edit']           = 'Informações da conta';
+$_['text_password']       = 'Modificar senha';
+$_['text_address']        = 'Endereços';
+$_['text_credit_card']    = 'Cartões de crédito';
+$_['text_wishlist']       = 'Lista de desejos';
+$_['text_order']          = 'Histórico de pedidos';
+$_['text_download']       = 'Downloads';
+$_['text_reward']         = 'Pontos';
+$_['text_return']         = 'Devoluções';
+$_['text_transaction']    = 'Transações';
+$_['text_newsletter']     = 'Novidades, ofertas e promoções por e-mail';
+$_['text_recurring']      = 'Assinaturas';
+$_['text_transactions']   = 'Transações';
+$_['text_affiliate_add']  = 'Registe-se para uma conta de afiliado';
+$_['text_affiliate_edit'] = 'Edite as suas informações de afiliado';
+$_['text_tracking']       = 'Personalizar afiliado tracking code';
