694d153a524a88f1d1f20af0b80ac9e26f2159db
Marco Ribeiro <marco.lob@gmail.com>
2021-02-20 15:39:32 +0000

    removed pt-br catalog

--- catalog/language/pt-br/mail/transaction.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Text
-$_['text_subject']  = '%s - Nova transação em sua conta';
-$_['text_received'] = 'Sua conta na loja %s, teve um registro de transação.';
-$_['text_amount']   = 'Valor da transação:';
-$_['text_total']    = 'Saldo atual:';
